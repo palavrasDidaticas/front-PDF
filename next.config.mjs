cb2e4b2d9063815f07e3059c63007a9e4e919ecd
palavrasDidaticas <167830279+palavrasDidaticas@users.noreply.github.com>
2025-03-27 21:32:17 -0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["https://back-eight-chi.vercel.app"],
+    domains: ["media.gettyimages.com"],
+  },
+};
+
+export default nextConfig;
